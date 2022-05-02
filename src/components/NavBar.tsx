25754484ac9e538bc6837b6428b1d9cb5bf202f7
Nikitapil <nikitapiliavets@MacBook-Pro-Nikita.local>
2022-05-02 14:38:27 +0300

    fix sidebar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -39,63 +39,65 @@ export const NavBar: FC = () => {
     <header className="header container">
       <h1>Welcome {userName || "Guest"}</h1>
       <nav className="nav-bar">
-        <ul className={`nav-bar__links ${isMobileMenuActive && "nav-active"}`}>
-          <li onClick={() => setIsMobileMenuActive(false)}>
-            <NavLink
-              to={"/"}
-              className={({ isActive }) =>
-                isActive ? "nav-bar__btn nav-bar__btn-active" : "nav-bar__btn "
-              }
-            >
-              Main
-            </NavLink>
-          </li>
-          <li onClick={() => setIsMobileMenuActive(false)}>
-            <NavLink
-              to={"/customquizes"}
-              className={({ isActive }) =>
-                isActive ? "nav-bar__btn nav-bar__btn-active" : "nav-bar__btn "
-              }
-            >
-              Custom quizes
-            </NavLink>
-          </li>
-          {!userId && (
-            <li onClick={() => setIsMobileMenuActive(false)}>
-              <button onClick={changeSignInModalState} className="nav-bar__btn">
-                Sign in
-              </button>
-            </li>
-          )}
-          {!userId && (
-            <li onClick={() => setIsMobileMenuActive(false)}>
-              <button onClick={changeSignUpModalState} className="nav-bar__btn">
-                Sign up
-              </button>
-            </li>
-          )}
-          {userId && (
+        <div className={`nav-bar__links ${isMobileMenuActive && "nav-active"}`} onClick={() => setIsMobileMenuActive(false)}>
+          <ul className="nav-bar__list" onClick={(e) => e.stopPropagation()}>
             <li onClick={() => setIsMobileMenuActive(false)}>
               <NavLink
-                to={"/profile/info"}
+                to={"/"}
                 className={({ isActive }) =>
-                  isActive
-                    ? "nav-bar__btn nav-bar__btn-active"
-                    : "nav-bar__btn "
+                  isActive ? "nav-bar__btn nav-bar__btn-active" : "nav-bar__btn "
                 }
               >
-                Profile
+                Main
               </NavLink>
             </li>
-          )}
-          {userId && (
             <li onClick={() => setIsMobileMenuActive(false)}>
-              <button className="nav-bar__btn" onClick={logout}>
-                Sign out
-              </button>
+              <NavLink
+                to={"/customquizes"}
+                className={({ isActive }) =>
+                  isActive ? "nav-bar__btn nav-bar__btn-active" : "nav-bar__btn "
+                }
+              >
+                Custom quizes
+              </NavLink>
             </li>
-          )}
-        </ul>
+            {!userId && (
+              <li onClick={() => setIsMobileMenuActive(false)}>
+                <button onClick={changeSignInModalState} className="nav-bar__btn">
+                  Sign in
+                </button>
+              </li>
+            )}
+            {!userId && (
+              <li onClick={() => setIsMobileMenuActive(false)}>
+                <button onClick={changeSignUpModalState} className="nav-bar__btn">
+                  Sign up
+                </button>
+              </li>
+            )}
+            {userId && (
+              <li onClick={() => setIsMobileMenuActive(false)}>
+                <NavLink
+                  to={"/profile/info"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "nav-bar__btn nav-bar__btn-active"
+                      : "nav-bar__btn "
+                  }
+                >
+                  Profile
+                </NavLink>
+              </li>
+            )}
+            {userId && (
+              <li onClick={() => setIsMobileMenuActive(false)}>
+                <button className="nav-bar__btn" onClick={logout}>
+                  Sign out
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
         <button
           className={`mobile-btn ${isMobileMenuActive && "nav-active"}`}
           onClick={() => setIsMobileMenuActive(!isMobileMenuActive)}
